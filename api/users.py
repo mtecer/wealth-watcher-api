42174b8ae6d684b23ec972ba2d173b789b402de9
Mehmet Tecer <mehmettecer@gmail.com>
2023-09-22 19:13:03 -0400

    Adds POST, DELETE, GET methods.

--- api/users.py
+++ api/users.py
@@ -1,47 +1,92 @@
 import json
 
 from fastapi import APIRouter
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, PrivateAttr
 from uuid import uuid4
 
 from config import read_users, write_users
 
+def generate_uuid4() -> str:
+    return str(uuid4())
+
 class User(BaseModel):
-    id: uuid4
+    # id: uuid4 = Field(default_factory=get_new_uuid)
+    # _id: uuid4 = PrivateAttr(default_factory=lambda: uuid4().hex)
+    _id: uuid4 = PrivateAttr(default_factory=generate_uuid4)
     user_name: str = None
     name: str | None = None
 
+    def model_dump_dict(self):
+        return {
+            "id": self._id,
+            "user_name": self.user_name,
+            "name": self.name
+        }
+
 router = APIRouter()
 
-USERS = [
-    {
-        "id": "91cf6a87-c449-4940-b10d-5c792e03657d",
-        "user_name": "user_1",
-        "name": "User One"
-    },
-    {
-        "id": "5d854cd3-2920-495a-8b8e-f5e4887e7db8",
-        "user_name": "user_2",
-        "name": "User Two"
-    },
-    {
-        "id": "d98acc31-223b-46e7-9664-dea41e8b091c",
-        "user_name": "user_3",
-        "name": "User Three"
-    }
-]
+# USERS = [
+#     {
+#         "id": "91cf6a87-c449-4940-b10d-5c792e03657d",
+#         "user_name": "user_1",
+#         "name": "User One"
+#     },
+#     {
+#         "id": "5d854cd3-2920-495a-8b8e-f5e4887e7db8",
+#         "user_name": "user_2",
+#         "name": "User Two"
+#     },
+#     {
+#         "id": "d98acc31-223b-46e7-9664-dea41e8b091c",
+#         "user_name": "user_3",
+#         "name": "User Three"
+#     }
+# ]
 
-@router.get("/api/users")
+# Get all users
+@router.get("/api/users", status_code=200)
 async def get_users():
     return read_users()
 
-@router.get("/api/users/{user_name}")
-async def get_user(user_name: str):
-    return read_users()
-
-@router.post("/api/users/")
-async def craete_user(user: User):
+# Get user by user_name
+@router.get("/api/users/{user_name}", status_code=200)
+async def get_user(*, user_name: str):
     users = read_users()
-    users.append(json.loads(user.model_dump_json()))
+    result = [ user for user in users if user["user_name"] == user_name ]
+    return result if result else []
+
+# Seaerch a user
+@router.get("/api/users/search/", status_code=200)
+async def search_user(keyword: str | None = None):
+    users = read_users()
+    result = [ user for user in users if keyword.lower() in user["user_name"].lower() ]
+    return result
+
+# Create a user
+@router.post("/api/users/", status_code=201)
+async def create_user(user: User):
+    users = read_users()
+    # users.append({
+    #     # "id": str(user._id),
+    #     "id": user._id,
+    #     "user_name": user.user_name,
+    #     "name": user.name
+    # })
+    users.append(user.model_dump_dict())
+    # print(user)
+    # print(user.model_dump_json())
+    # print(user.model_dump())
+    # print(user.model_dump_dict())
     write_users(users)
-    return user
+    # return user
+    return user.model_dump_dict()
+
+# Delete user by user_name
+@router.delete("/api/users/{user_name}", status_code=204)
+async def delete_user(*, user_name: str):
+    users = read_users()
+    result = [ user for user in users if user["user_name"] != user_name ]
+    write_users(result)
+    # return result
+
+# Update user by user id
--- main.py
+++ main.py
@@ -20,53 +20,10 @@ def configure_routing():
 def configure():
     configure_routing()
     
-    # base_dir = Path(__file__).resolve().parent
-    # data_dir = Path(module_dir / "data")
-    # file_path = Path(data_dir / "users.json")
-    # file2_path = Path(data_dir / "users-2.json")
-    
-    # users_datafile = Path(base_dir / "data" / "users.json")
-    # users = json.loads(users_datafile.read_text())
-
-    # print(type(users))
-    # users.append({
-    #     "id": "e77acc31-223b-46e7-9664-dea41e8b091c",
-    #     "user_name": "user_4",
-    #     "name": "User Four"
-    # })
-    # users_datafile.write_text(json.dumps(users, indent=4))
-
-    # pprint(users, indent=4)
-    # content = file_path.read_text()
-    # content_dict = json.loads(content)
-
-    # pprint(content_dict, indent=4)
-    # content_dict.append({
-    #     "id": "e77acc31-223b-46e7-9664-dea41e8b091c",
-    #     "user_name": "user_4",
-    #     "name": "User Four"
-    # })
-    # print(type(content_dict))
-    # print(content_dict)
-    # file2_path.write_text(json.dumps(content_dict, indent=4))
-
-    # print(module_dir)
-    # print(data_dir)
-
-    # print(Path.cwd())
-    # print(Path(__file__))
-    # print(Path(__file__).resolve())
-    # print(Path(__file__).resolve().stem)
-    # print(Path(__file__).stem) # main
-    # print(Path(__file__).name) # main.py
-    # print(Path(__file__).basename)
-    # print(Path(__file__).resolve().parent) # /Users/mtecer/Projects/wealth-watcher-api
-    # print(Path(__file__).resolve().parent.parent) # /Users/mtecer/Projects
-
 if __name__ == "__main__":
     configure()
     # users = read_users()
-    uvicorn.run(api, host="127.0.0.1", port=8081)
+    uvicorn.run(api, host="127.0.0.1", port=8081, reload=True)
 else:
     configure()
     # users = read_users()
